fca8fd41e5e9bc1fef7208ac629399984a444a05
JasonPob <jasonpob@gmail.com>
2019-04-16 12:03:06 -0400

    added all individual functions

--- liri.js
+++ liri.js
@@ -39,30 +39,30 @@ var axios = require("axios");
 // }
 
 
-//functions
-var choose = function (caseData, functionData) {
-    switch (caseData) {
-        case 'concert-this':
-            bandSearch(search);
-            break;
-        case "spotify-this-song":
-            getSpotify(functionData);
-            break;
-        case "movie-this":
-            getMovie(functionData);
-            break;
-        case "do-what-it-says":
-            doWhatItSays();
-            break;
-        default:
-            console.log("LIRI doesn't know");
-    }
-};
+// //functions
+// var choose = function (caseData, functionData) {
+//     switch (caseData) {
+//         case 'concert-this':
+//             artistSearch(artist);
+//             break;
+//         case "spotify-this-song":
+//             getSpotify(functionData);
+//             break;
+//         case "movie-this":
+//             getMovie(functionData);
+//             break;
+//         case "do-what-it-says":
+//             doWhatItSays();
+//             break;
+//         default:
+//             console.log("LIRI doesn't know");
+//     }
+// };
 
-var runThis = function (argOne, argTwo) {
-    choose(argOne, argTwo);
-};
-runThis(process.argv[2], process.argv[3]);
+// var runThis = function (argOne, argTwo) {
+//     choose(argOne, argTwo);
+// };
+// runThis(process.argv[2], process.argv[3]);
 
 
 //Concert-this
@@ -82,4 +82,107 @@ var artistSearch = (artist) => {
 			console.log(error);
 		});
 }
+
+//Spotifiy 
+var getArtistNames = function (artist) {
+    return artist.name;
+};
+
+//spotify api
+
+var getSpotify = function (songName) {
+    if (songName === undefined) {
+        songName = "Yellow";
+    }
+
+    spotify.search(
+        {
+            type: "track",
+            query: songName
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }
+
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log(i);
+                console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+                console.log("song name: " + songs[i].name);
+                console.log("preview song: " + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+                console.log("-----------------------------------");
+            }
+        }
+    );
+};
+
+//OMDB
+var getMovie = function (movieName) {
+    if (movieName === undefined) {
+        movieName = "Mr Nobody";
+    }
+
+    var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+
+    request(urlHit, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var jsonData = JSON.parse(body);
+
+            console.log("Title: " + jsonData.Title);
+            console.log("Year: " + jsonData.Year);
+            console.log("Rated: " + jsonData.Rated);
+            console.log("IMDB Rating: " + jsonData.imdbRating);
+            console.log("Country: " + jsonData.Country);
+            console.log("Language: " + jsonData.Language);
+            console.log("Plot: " + jsonData.Plot);
+            console.log("Actors: " + jsonData.Actors);
+            console.log("Rotton Tomatoes Rating: " + jsonData.Ratings[1].Value);
+        }
+    });
+};
+
+//do-what-it-says
+var doWhatItSays = function () {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        console.log(data);
+
+        var dataArr = data.split(",");
+
+        if (dataArr.length === 2) {
+            choose(dataArr[0], dataArr[1]);
+        }
+        else if (dataArr.length === 1) {
+            choose(dataArr[0]);
+        }
+    });
+};
+
+//functions
+var choose = function (caseData, functionData) {
+    switch (caseData) {
+        case 'concert-this':
+            artistSearch(artist);
+            break;
+        case "spotify-this-song":
+            getSpotify(functionData);
+            break;
+        case "movie-this":
+            getMovie(functionData);
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+        default:
+            console.log("LIRI doesn't know");
+    }
+};
+
+var runThis = function (argOne, argTwo) {
+    choose(argOne, argTwo);
+};
+runThis(process.argv[2], process.argv[3]);
 
